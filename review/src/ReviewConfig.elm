8355a4dc4ff7aac948a023445b83734c9a538781
Phill Sparks <me@phills.me.uk>
2020-06-13 08:58:18 +0100

    Update review rules

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,41 +1,57 @@
 module ReviewConfig exposing (config)
 
-{-| Do not rename the ReviewConfig module or the config function, because
-`elm-review` will look for these.
-
-To add packages that contain rules, add them to this review project using
-
-    `elm install author/packagename`
-
-when inside the directory containing this file.
-
--}
-
+import NoAlways
+import NoBooleanCase
 import NoDebug.Log
 import NoDebug.TodoOrToString
 import NoExposingEverything
+import NoForbiddenWords
 import NoImportingEverything
+import NoLeftPizza
 import NoMissingTypeAnnotation
+import NoRedundantConcat
+import NoRedundantCons
 import NoUnused.CustomTypeConstructors
 import NoUnused.Dependencies
 import NoUnused.Exports
 import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
 import NoUnused.Variables
-import Review.Rule exposing (Rule)
+import Review.Rule as Rule exposing (Rule)
 import UseCamelCase
+import Vendor.NoFullyAppliedPrefixOperator as NoFullyAppliedPrefixOperator
 
 
 config : List Rule
 config =
-    [ NoDebug.Log.rule
+    [ NoAlways.rule
+    , NoBooleanCase.rule
+    , NoDebug.Log.rule
     , NoDebug.TodoOrToString.rule
     , NoExposingEverything.rule
+    , NoForbiddenWords.rule
+        [ "- [ ]"
+        , "TODO"
+        ]
+    , NoFullyAppliedPrefixOperator.rule
     , NoImportingEverything.rule []
+    , NoLeftPizza.rule
+        |> Rule.ignoreErrorsForDirectories
+            [ -- Test functions are traditionally built up using a left pizza.
+              -- While we don't want them in our regular code, let's allow them
+              -- just for tests.
+              "tests/"
+            ]
     , NoMissingTypeAnnotation.rule
+    , NoRedundantConcat.rule
+    , NoRedundantCons.rule
     , NoUnused.CustomTypeConstructors.rule []
     , NoUnused.Dependencies.rule
     , NoUnused.Exports.rule
     , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
     , NoUnused.Variables.rule
     , UseCamelCase.rule UseCamelCase.default
     ]
